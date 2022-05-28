340eb843a974d9aae898fe26f7e1d8ce01d475f7
Azrael <66616229+WDNMD-S@users.noreply.github.com>
2022-05-28 14:59:03 +0800

    Update HelloGit.java

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hotfix add!");
+        System.out.println("test add!");
     }
 }
