528d06650daae8584fb60cac770f0124cc28595a
WDNMD-S <github.com/WDNMD-S>
2022-05-28 14:57:20 +0800

    合并提交

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,6 +1,7 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("master add!");
         System.out.println("hotfix add!");
     }
 }
